72a0c7b74dda442c2d582f254c3a8e72af04aa2d
vgrau98 <victor.grau93@gmail.com>
2021-12-23 11:20:27 +0100

    first commit

---
+++ src/test/java/com/insa/projet/microservices/temperature/TemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insa.projet.microservices.temperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
