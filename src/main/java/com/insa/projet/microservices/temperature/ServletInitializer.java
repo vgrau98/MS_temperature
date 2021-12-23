78c133e213e21377efab10a346e7cec6e358aa92
vgrau98 <victor.grau93@gmail.com>
2021-12-23 22:36:22 +0100

    test

--- src/main/java/com/insa/projet/microservices/temperature/ServletInitializer.java
+++ src/main/java/com/insa/projet/microservices/temperature/ServletInitializer.java
@@ -1,8 +1,11 @@
 package com.insa.projet.microservices.temperature;
 
+import javax.servlet.annotation.WebServlet;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@WebServlet("/test")
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
