cb06254b12f42eb881f93793c39520a5b1d1bb72
vgrau98 <victor.grau93@gmail.com>
2021-12-23 22:53:48 +0100

    test

--- src/main/java/com/insa/projet/microservices/temperature/TemperatureApplication.java
+++ src/main/java/com/insa/projet/microservices/temperature/TemperatureApplication.java
@@ -1,13 +1,20 @@
 package com.insa.projet.microservices.temperature;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class TemperatureApplication {
-	
+public class TemperatureApplication extends SpringBootServletInitializer{
 
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TemperatureApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TemperatureApplication.class, args);
 	}
