71e46f1eb5135f7d940155dbf786c8e9065fb892
vgrau98 <victor.grau93@gmail.com>
2021-12-24 01:29:06 +0100

    test

--- src/main/java/com/insa/projet/microservices/temperature/controller/temperatureRessource.java
+++ src/main/java/com/insa/projet/microservices/temperature/controller/temperatureRessource.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/temperature")
 public class temperatureRessource {
 
 	public DataBase db = new DataBase();
